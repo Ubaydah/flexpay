3b9c84a8824677bf2efc337787581d3d6cf59b51
Ubaydah <abdulwasiuubaydah@gmail.com>
2022-09-06 11:26:17 +0100

    registered employee profile on admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CustomUser, CompanyProfile
+from .models import CustomUser, CompanyProfile, EmployeeeProfile
 
 
 admin.site.register(CustomUser)
 admin.site.register(CompanyProfile)
+admin.site.register(EmployeeeProfile)
